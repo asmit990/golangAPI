b7a1133e53a8d39b29c5f6ce0c5cdb488fdf15c2
asmit990 <asmitpandeyw4354@gmail.com>
2026-02-13 03:56:44 +0530

    everything is ready

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
+
+	"github.com/jackc/pgx/v5"
+
+	"github.com/asmit990/golangAPI/internal/env"
 )
 
 func main() {
+	ctx := context.Background()
 	cfg := config{
 		addr: ":8080",
-		db:   dbConfig{},
-	}
-
-	api := application{
-		config: cfg,
+		db: dbConfig{
+			dsn: env.GetString("GOOSE_DBSTRING", "user=postgres password=postgres dbname=app sslmode=disable"),
+		},
 	}
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
+
+	conn, err := pgx.Connect(ctx, cfg.db.dsn)
+	if err != nil {
+		panic(err)
+
+	}
+	defer conn.Close(ctx)
+
+	// database
+	logger.Info("connected to database", "dsn", cfg.db.dsn)
+	api := application{
+		config: cfg,
+	}
 
 	h := api.mount()
 	if err := api.run(h); err != nil {
